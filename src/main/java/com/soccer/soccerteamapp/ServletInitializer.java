544117ff7b844feb65905c5007ea87c8054b19cc
davidbryan <aviles.davidb@gmail.com>
2021-11-09 15:55:15 -0500

    second commit with Spring boot set up

---
+++ src/main/java/com/soccer/soccerteamapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.soccer.soccerteamapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SoccerteamAppApplication.class);
+    }
+
+}
---
+++ src/main/java/com/soccer/soccerteamapp/SoccerteamAppApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.soccerteamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerteamAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerteamAppApplication.class, args);
+    }
+
+}
